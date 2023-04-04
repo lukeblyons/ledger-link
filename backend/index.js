ac6fc6e9eadd18f6b87603b9fd287a0650efe7fb
lukeblyons <lukeblyons@gmail.com>
2023-04-04 17:53:01 -0600

    Server works, just annoying

---
+++ backend/index.js
@@ -0,0 +1,110 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+// create the Express app
+const app = express();
+// parse incoming form data
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+// handle signup form submission
+app.post('/signup', (req, res) => {
+    const username = req.body.username;
+    const email = req.body.email;
+    const password = req.body.password;
+  
+    // TODO: handle creating a new user account
+  
+    res.send('Signup successful');
+  });
+  
+  // handle login form submission
+  app.post('/login', (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+  
+    // TODO: handle logging in an existing user
+  
+    res.send('Login successful');
+  });
+  // serve static assets like CSS and JS files
+
+
+const PORT = 3000;
+
+
+app.get('/client/html/landing.html', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/html/landing.html'));
+})
+
+app.get('/client/html/dashboard.html', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/html/dashboard.html'));
+})
+
+app.get('/client/html/otp.html', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/html/otp.html'));
+})
+
+// CSS
+app.get('/client/css/buttons.css', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/css/buttons.css'));
+});
+
+app.get('/client/css/cards.css', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/css/cards.css'));
+});
+
+app.get('/client/css/dashboard.css', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/css/dashboard.css'));
+});
+
+app.get('/client/css/logo.css', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/css/logo.css'));
+});
+
+app.get('/client/css/otp.css', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/css/otp.css'));
+});
+
+app.get('/client/css/signup-form.css', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/css/signup-form.css'));
+});
+
+app.get('/client/css/star.css', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/css/star.css'));
+});
+
+app.get('/client/css/stocks.css', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/css/stocks.css'));
+});
+
+// JS
+app.get('/client/frontend-js/buttons.js', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/frontend-js/buttons.js'));
+});
+
+app.get('/client/frontend-js/dashboard.js', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/frontend-js/dashboard.js'));
+});
+
+app.get('/client/frontend-js/otp.js', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/frontend-js/otp.js'));
+});
+
+app.get('/client/frontend-js/signup-form.js', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/frontend-js/signup-form.js'));
+});
+
+app.get('/client/frontend-js/star.js', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/frontend-js/star.js'));
+});
+
+app.get('/client/frontend-js/stocks.js', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/frontend-js/stocks.js'));
+});
+
+
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
---
+++ client/frontend-js/stocks.js
@@ -0,0 +1,110 @@
+const API_KEY = '6OQU5XY1RI3KONVS';
+const API_BASE_URL = 'https://www.alphavantage.co/query?';
+
+document.getElementById('search-form').addEventListener('submit', (e) => {
+  e.preventDefault();
+  const stockTicker = document.getElementById('stock-ticker').value.toUpperCase();
+  addStockToFavoriteList(stockTicker);
+});
+
+async function addStockToFavoriteList(stockTicker) {
+  // Set default tickers
+  const defaultTickers = ['AAPL', 'META', 'TSLA'];
+  
+  // If the stock ticker is not provided, use default tickers
+  if (!stockTicker) {
+    for (const ticker of defaultTickers) {
+      await addStockToFavoriteList(ticker);
+    }
+    return;
+  }
+  
+  const stockInfo = await getStockInfo(stockTicker);
+  if (!stockInfo) return;
+  const favoritesList = document.getElementById('favorites-list').getElementsByTagName('tbody')[0];
+
+  const newRow = favoritesList.insertRow();
+  newRow.innerHTML = `
+    <td>${stockInfo.ticker}</td>
+    <td>${stockInfo.name}</td>
+    <td>${stockInfo.price}</td>
+    <td style="color: ${stockInfo.dailyChange >= 0 ? 'green' : 'red'}">${stockInfo.dailyChange} ${stockInfo.dailyChange >= 0 ? '<i class="fi fi-rr-angle-small-up"></i>' : '<i class="fi fi-rr-angle-small-down"></i>'}</td>
+    <td>${stockInfo.marketCap}</td>
+    <td>${stockInfo.dividendYield}</td>
+    <td><button onclick="deleteRow(this)">Delete</button></td>
+  `;
+
+  sortTable(favoritesList);
+}
+
+
+async function getStockInfo(stockTicker) {
+  try {
+    const overviewUrl = `${API_BASE_URL}function=OVERVIEW&symbol=${stockTicker}&apikey=${API_KEY}`;
+    const dailyUrl = `${API_BASE_URL}function=TIME_SERIES_DAILY_ADJUSTED&symbol=${stockTicker}&apikey=${API_KEY}`;
+
+    const [overviewResponse, dailyResponse] = await Promise.all([
+      fetch(overviewUrl),
+      fetch(dailyUrl),
+    ]);
+
+    const [overviewData, dailyData] = await Promise.all([
+      overviewResponse.json(),
+      dailyResponse.json(),
+    ]);
+
+    if (!overviewData.Name || !dailyData['Time Series (Daily)']) {
+      alert('Invalid stock ticker or error fetching data. Please try again.');
+      return null;
+    }
+
+    const dailyTimeSeries = dailyData['Time Series (Daily)'];
+    const latestDate = Object.keys(dailyTimeSeries)[0];
+    const latestData = dailyTimeSeries[latestDate];
+
+    const stockInfo = {
+      ticker: stockTicker,
+      name: overviewData.Name,
+      price: `$${parseFloat(latestData['4. close']).toFixed(2)}`,
+      dailyChange: `${(((parseFloat(latestData['4. close']) - parseFloat(latestData['1. open'])) / parseFloat(latestData['1. open'])) * 100).toFixed(2)}%`,
+      marketCap: formatMarketCap(parseInt(overviewData.MarketCapitalization)),
+      dividendYield: `${(parseFloat(overviewData.DividendYield) * 100).toFixed(2)}%`,
+    };
+
+    return stockInfo;
+  } catch (error) {
+    console.error('Error fetching stock info:', error);
+    return null;
+  }
+}
+
+
+
+function deleteRow(btn) {
+  const row = btn.parentNode.parentNode;
+  row.parentNode.removeChild(row);
+}
+
+function sortTable(tbody) {
+  const rows = Array.from(tbody.rows);
+  rows.sort((a, b) => a.cells[0].innerText.localeCompare(b.cells[0].innerText));
+  rows.forEach(row => tbody.appendChild(row));
+}
+
+function formatMarketCap(marketCap) {
+  const trillion = 1000000000000;
+  const billion = 1000000000;
+  const million = 1000000;
+
+  if (marketCap >= trillion) {
+    return `$${(marketCap / trillion).toFixed(2)}T`;
+  } else if (marketCap >= billion) {
+    return `$${(marketCap / billion).toFixed(2)}B`;
+  } else if (marketCap >= million) {
+    return `$${(marketCap / million).toFixed(2)}M`;
+  } else {
+    return `$${marketCap}`;
+  }
+}
+
+addStockToFavoriteList('');
