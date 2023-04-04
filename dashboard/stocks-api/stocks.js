c1841135fabbc1d5d7caf1738a237e69e59956f8
lukeblyons <lukeblyons@gmail.com>
2023-04-03 18:34:33 -0600

    Tried to fix the backend

--- dashboard/stocks-api/stocks.js
+++ dashboard/stocks-api/stocks.js
@@ -1,39 +1,38 @@
-// Event listeners
-document.getElementById('search-form').addEventListener('submit', async (event) => {
-  event.preventDefault();
-  const stockTicker = document.getElementById('stock-ticker').value.toUpperCase();
-  const stockData = await fetchStockData(stockTicker);
-  if (stockData) {
-    addToFavorites(stockData);
-  }
-});
+const searchForm = document.getElementById('search-form');
+const stockTickerInput = document.getElementById('stock-ticker');
+const favoritesList = document.getElementById('favorites-list').querySelector('tbody');
 
-// API-related functions
-async function fetchStockData(ticker) {
-  const response = await fetch(`https://financialmodelingprep.com/api/v3/quote/${ticker}?apikey=yourapikey`);
+const ALPHA_VANTAGE_API_KEY = '6OQU5XY1RI3KONVS';
+
+// Function to fetch stock data using Alpha Vantage API
+async function fetchStockData(stockTicker) {
+  const apiUrl = `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${stockTicker}&apikey=${ALPHA_VANTAGE_API_KEY}`;
+
+  const response = await fetch(apiUrl);
   const data = await response.json();
-  if (data.length === 0) {
-    alert('Invalid ticker symbol. Please try again.');
-    return null;
-  }
-  const stockData = {
-    ticker: data[0].symbol,
-    name: data[0].name,
-    price: data[0].price,
-    marketCap: data[0].marketCap,
-    dividendYield: data[0].lastAnnualDividend,
-    dailyData: null
-  };
-  const dailyResponse = await fetch(`https://financialmodelingprep.com/api/v3/historical-price-full/${ticker}?apikey=yourapikey`);
-  const dailyData = await dailyResponse.json();
-  if (dailyData.historical.length < 2) {
-    alert('Error fetching stock data. Please try again later.');
-    return null;
-  }
-  stockData.dailyData = dailyData.historical.slice(0, 2);
-  return stockData;
+  return data['Global Quote'];
 }
 
+// Add numberWithCommas and formatMarketCap functions
+function numberWithCommas(x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+function formatMarketCap(marketCap) {
+  const value = parseFloat(marketCap);
+
+  if (value >= 1e12) {
+    return (value / 1e12).toFixed(2) + 'T';
+  } else if (value >= 1e9) {
+    return (value / 1e9).toFixed(2) + 'B';
+  } else if (value >= 1e6) {
+    return (value / 1e6).toFixed(2) + 'M';
+  } else {
+    return numberWithCommas(value.toFixed(2));
+  }
+}
+
+// Calculate daily change
 function calculateDailyChange(dailyData) {
   const currentDayClose = parseFloat(dailyData[0].close);
   const previousDayClose = parseFloat(dailyData[1].close);
@@ -46,153 +45,44 @@ function calculateDailyChange(dailyData) {
   };
 }
 
+// Function to add stock to favorites list
+function addStockToFavorites(stockData) {
+  const newRow = favoritesList.insertRow();
+  newRow.insertCell().innerText = stockData['01. symbol'];
+  newRow.insertCell().innerText = 'N/A'; // Replace with stock name if available
+  newRow.insertCell().innerText = stockData['05. price'];
+  newRow.insertCell().innerText = stockData['09. change'];
+  newRow.insertCell().innerText = formatMarketCap('N/A'); // Replace with market cap if available
+  newRow.insertCell().innerText = 'N/A'; // Replace with dividend yield if available
+  newRow.insertCell().innerHTML = '<button class="remove-stock">Remove</button>';
 
-
-// Favorites-related functions
-async function addToFavorites(stockData, userId) {
-  const { percentageChange, arrowIcon, textColor } = calculateDailyChange(stockData.dailyData);
-  
-  // Add the stock to the user's favorites in the database
-  await addFavoriteStock(userId, stockData.ticker, stockData.name, stockData.price, percentageChange, stockData.marketCap, stockData.dividendYield);
-  
-  const favoritesList = document.getElementById('favorites-list').getElementsByTagName('tbody')[0];
-  const tableRow = document.createElement('tr');
-  tableRow.id = stock-${stockData.ticker};
-  
-  const tickerCell = document.createElement('td');
-  tickerCell.innerText = stockData.ticker;
-  tableRow.appendChild(tickerCell);
-  
-  const nameCell = document.createElement('td');
-  nameCell.innerText = stockData.name;
-  tableRow.appendChild(nameCell);
-  
-  const priceCell = document.createElement('td');
-  priceCell.innerText = $${numberWithCommas(parseFloat(stockData.price).toFixed(2))};
-  tableRow.appendChild(priceCell);
-  
-  const dailyChangeCell = document.createElement('td');
-  dailyChangeCell.innerHTML = <span style="color:${textColor};">${arrowIcon} ${percentageChange.toFixed(2)}%</span>;
-  tableRow.appendChild(dailyChangeCell);
-  
-  const marketCapCell = document.createElement('td');
-  marketCapCell.innerText = $${formatMarketCap(stockData.marketCap)};
-  tableRow.appendChild(marketCapCell);
-  
-  const dividendYieldCell = document.createElement('td');
-  dividendYieldCell.innerText = ${(parseFloat(stockData.dividendYield) * 100).toFixed(2)}%;
-  tableRow.appendChild(dividendYieldCell);
-  
-  const actionCell = document.createElement('td');
-  const deleteButton = document.createElement('button');
-  deleteButton.className = 'delete-button';
-  deleteButton.innerText = 'Delete';
-  deleteButton.addEventListener('click', async () => {
-  await removeFavoriteStock(stockData.ticker); // Use the stockId instead of the ticker once you have it
-  deleteFromFavorites(stockData.ticker);
+  // Add event listener to the remove button
+  newRow.querySelector('.remove-stock').addEventListener('click', () => {
+    newRow.remove();
+    // Add logic to remove stock from the user's favorites list in the database
   });
-  actionCell.appendChild(deleteButton);
-  tableRow.appendChild(actionCell);
-  
-  favoritesList.appendChild(tableRow);
-  
-  sortFavoritesList();
+}
+
+// Event listener for the search form
+searchForm.addEventListener('submit', async (event) => {
+  event.preventDefault();
+
+  const stockTicker = stockTickerInput.value.toUpperCase();
+  const stockData = await fetchStockData(stockTicker);
+
+  if (stockData) {
+    addStockToFavorites(stockData);
+    // Add logic to save stock to the user's favorites list in the database
+  } else {
+    alert('Stock not found');
   }
-  
-  function deleteFromFavorites(ticker) {
-  const listItem = document.getElementById(stock-${ticker});
-  listItem.remove();
-  }
-  
-  function sortFavoritesList() {
-  const favoritesList = document.getElementById('favorites-list').getElementsByTagName('tbody')[0];
-  const rows = Array.from(favoritesList.getElementsByTagName('tr'));
-  
-  rows.sort((a, b) => {
-  const tickerA = a.cells[0].innerText;
-  const tickerB = b.cells[0].innerText;
-  return tickerA.localeCompare(tickerB);
 });
 
-favoritesList.innerHTML = '';
-
-for (const row of rows) {
-favoritesList.appendChild(row);
-}
-}
-
-
-
-// Helper functions
-function numberWithCommas(x) {
-  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+// Event listener for removing stocks from the favorites list
+favoritesList.addEventListener('click', (event) => {
+  if (event.target.classList.contains('remove-stock')) {
+    const stockRow = event.target.parentNode.parentNode;
+    stockRow.remove();
+    // Add logic to remove stock from the user's favorites list in the database
   }
-  
-  function formatMarketCap(marketCap) {
-  const value = parseFloat(marketCap);
-  
-  if (value >= 1e12) {
-  return (value / 1e12).toFixed(2) + 'T';
-  } else if (value >= 1e9) {
-  return (value / 1e9).toFixed(2) + 'B';
-  } else if (value >= 1e6) {
-  return (value / 1e6).toFixed(2) + 'M';
-  } else {
-  return numberWithCommas(value.toFixed(2));
-  }
-  }
-  
-  (async function loadFavorites() {
-  const userId = 'your_user_id'; // Replace this with the actual user ID after implementing authentication
-  const favoriteStocks = await getFavoriteStocks(userId);
-  
-  for (const stockData of favoriteStocks) {
-  addToFavorites(stockData, userId);
-  }
-  })();
-  
-  async function addFavoriteStock(userId, ticker, name, price, percentageChange, marketCap, dividendYield) {
-  try {
-  const response = await fetch('your-add-favorite-api-url', {
-  method: 'POST',
-  body: JSON.stringify({ userId, ticker, name, price, percentageChange, marketCap, dividendYield }),
-  headers: {
-  'Content-Type': 'application/json'
-  }
-  });
-
-  if (!response.ok) {
-    throw new Error('Error adding favorite stock');
-  }
-} catch (error) {
-  console.error('Error:', error);
-  }
-  }
-  
-  async function removeFavoriteStock(stockId) {
-  try {
-  const response = await fetch(your-remove-favorite-api-url/${stockId}, {
-  method: 'DELETE'
-  });
-
-  if (!response.ok) {
-    throw new Error('Error removing favorite stock');
-  }
-  } catch (error) {
-console.error('Error:', error);
-}
-}
-
-async function getFavoriteStocks(userId) {
-try {
-const response = await fetch(your-get-favorite-api-url/${userId});
-if (!response.ok) {
-throw new Error('Error getting favorite stocks');
-}
-const favoriteStocks = await response.json();
-return favoriteStocks;
-} catch (error) {
-console.error('Error:', error);
-return [];
-}
-}
+});
--- universal/forms/signup-form.js
+++ universal/forms/signup-form.js
@@ -3,110 +3,91 @@ const passwordToggle = document.querySelector('#password-toggle');
 const passwordInputLogin = document.querySelector('#password-input-login');
 const passwordToggleLogin = document.querySelector('#password-toggle-login');
 
-passwordToggle.addEventListener('click', function() {
-	if (passwordInput.type === 'password') {
-		passwordInput.type = 'text';
-		passwordToggle.classList.remove('fi-rs-lock-filled');
-		passwordToggle.classList.add('fi-rs-unlock');
-	} else {
-		passwordInput.type = 'password';
-		passwordToggle.classList.remove('fi-rs-unlock');
-		passwordToggle.classList.add('fi-rs-lock-filled');
-	}
+passwordToggle.addEventListener('click', function () {
+  if (passwordInput.type === 'password') {
+    passwordInput.type = 'text';
+    passwordToggle.classList.remove('fi-rs-lock-filled');
+    passwordToggle.classList.add('fi-rs-unlock');
+  } else {
+    passwordInput.type = 'password';
+    passwordToggle.classList.remove('fi-rs-unlock');
+    passwordToggle.classList.add('fi-rs-lock-filled');
+  }
 });
 
-passwordToggleLogin.addEventListener('click', function() {
-	if (passwordInputLogin.type === 'password') {
-		passwordInputLogin.type = 'text';
-		passwordToggleLogin.classList.remove('fi-rs-lock');
-		passwordToggleLogin.classList.add('fi-rs-unlock');
-	} else {
-		passwordInputLogin.type = 'password';
-		passwordToggleLogin.classList.remove('fi-rs-unlock');
-		passwordToggleLogin.classList.add('fi-rs-lock');
-	}
+passwordToggleLogin.addEventListener('click', function () {
+  if (passwordInputLogin.type === 'password') {
+    passwordInputLogin.type = 'text';
+    passwordToggleLogin.classList.remove('fi-rs-lock');
+    passwordToggleLogin.classList.add('fi-rs-unlock');
+  } else {
+    passwordInputLogin.type = 'password';
+    passwordToggleLogin.classList.remove('fi-rs-unlock');
+    passwordToggleLogin.classList.add('fi-rs-lock');
+  }
 });
-
-const form = document.querySelector('.form-signup');
-const loginForm = document.querySelector('.form-login');
-
-// Modified signup form event listener
-form.addEventListener('submit', async (event) => {
-	event.preventDefault();
-
-	const formData = new FormData(form);
-
-	const username = formData.get('username');
-	const email = formData.get('email');
-	const password = formData.get('password');
-
-	try {
-		const response = await fetch('http://localhost:3000/register', {
-			method: 'POST',
-			body: JSON.stringify({
-				username,
-				email,
-				password
-			}),
-			headers: {
-				'Content-Type': 'application/json'
-			}
-		});
-		const data = await response.json();
-
-		if (response.ok) {
-			window.location.href = '/universal/OTP/otp.html';
-		} else {
-			console.error(data.message);
-		}
-	} catch (error) {
-		console.error('Error:', error);
-	}
-});
-
-// Modified login form event listener
-loginForm.addEventListener('submit', async (event) => {
-	event.preventDefault();
-
-	const loginFormData = new FormData(loginForm);
-
-	const username = loginFormData.get('username');
-	const password = loginFormData.get('password');
-
-	try {
-		const response = await fetch('http://localhost:3000/login', {
-			method: 'POST',
-			body: JSON.stringify({
-				username,
-				password
-			}),
-			headers: {
-				'Content-Type': 'application/json'
-			}
-		});
-		const data = await response.json();
-
-		if (response.ok) {
-			window.location.href = '/dashboard/dashboard.html';
-		} else {
-			console.error(data.message);
-		}
-	} catch (error) {
-		console.error('Error:', error);
-	}
-});
-
 
 // Switch from signup to login
-document.querySelector(".switch-to-login").addEventListener("click", (e) => {
-	e.preventDefault();
-	document.querySelector(".signup-form").style.display = "none";
-	document.querySelector(".login-form").style.display = "block";
+document.querySelector('.switch-to-login').addEventListener('click', (e) => {
+  e.preventDefault();
+  document.querySelector('.signup-form').style.display = 'none';
+  document.querySelector('.login-form').style.display = 'block';
 });
 
 // Switch from login to signup
-document.querySelector(".switch-to-signup").addEventListener("click", (e) => {
-	e.preventDefault();
-	document.querySelector(".signup-form").style.display = "block";
-	document.querySelector(".login-form").style.display = "none";
+document.querySelector('.switch-to-signup').addEventListener('click', (e) => {
+  e.preventDefault();
+  document.querySelector('.signup-form').style.display = 'block';
+  document.querySelector('.login-form').style.display = 'none';
 });
+
+
+
+// Add event listeners for the signup and login forms
+document.querySelector('.form-signup').addEventListener('submit', async (event) => {
+	event.preventDefault();
+  
+	const username = event.target.querySelector('input[type="text"]').value;
+	const email = event.target.querySelector('input[type="email"]').value;
+	const password = event.target.querySelector('input[type="password"]').value;
+  
+	const response = await fetch('/register', {
+	  method: 'POST',
+	  headers: {
+		'Content-Type': 'application/json',
+	  },
+	  body: JSON.stringify({ username, email, password }),
+	});
+  
+	const data = await response.json();
+	if (response.status === 201) {
+	  alert(data.message);
+	  // Redirect to the dashboard or login page
+	} else {
+	  alert(data.error);
+	}
+  });
+  
+  document.querySelector('.form-login').addEventListener('submit', async (event) => {
+	event.preventDefault();
+  
+	const email = event.target.querySelector('input[type="text"]').value;
+	const password = event.target.querySelector('input[type="password"]').value;
+  
+	const response = await fetch('/login', {
+	  method: 'POST',
+	  headers: {
+		'Content-Type': 'application/json',
+	  },
+	  body: JSON.stringify({ email, password }),
+	});
+  
+	const data = await response.json();
+	if (response.status === 200) {
+	  alert(data.message);
+	  // Redirect to the dashboard
+	} else {
+	  alert(data.error);
+	}
+  });
+  
